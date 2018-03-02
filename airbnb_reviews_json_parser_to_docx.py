b73ee96666f8216703b92b3fe669fafe33717b33
Julia Milanese <33423699+juliamendoim@users.noreply.github.com>
2018-03-02 10:13:07 -0300

    Add files via upload

---
+++ airbnb_reviews_json_parser_to_docx.py
@@ -0,0 +1,41 @@
+import json
+import os
+import codecs
+from docx import Document
+ 
+datos_path = "path/reviews/"
+
+
+ #Loopeo por cada archivo
+ #data es un documento con 7 comentarios (adentro de reviews)
+for file in os.listdir(datos_path):
+	data_file = open(datos_path+file)
+	data = json.load(data_file)
+
+	i = 0
+
+	
+#Extrae cada comentario, lenguaje en el que está el comentario y id del usuario que comentó. Los escribe en un doc.	
+	documento = Document()
+	
+	for elemento in data["reviews"]:
+
+		x = (data["reviews"][i]["comments"])
+		y = (data["reviews"][i]["language"])
+		z = (data["reviews"][i]["id"])
+		
+		documento.add_paragraph(str(x))
+		documento.add_paragraph(detect_lan(str(y)))
+		documento.add_paragraph(str(z))
+		
+		i += 1
+	documento.save(str('reviewsdocx/'+str(file[0:-5])+'.docx'))
+
+#O, se pueden volcar en un .txt (acá, filtra por idioma)
+if y == 'es':
+			with codecs.open('/path/airbnb/txt/'+str(file[0:-5])+str(i)+'.txt','w', 'utf-8') as f:
+				f.write(str(x))
+
+
+
+	
---
+++ airbnb_scrapper.py
@@ -0,0 +1,65 @@
+#librería de testing (para ir abriendo los links) y de parseo de html
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import json
+import re
+import codecs
+
+#Abre chromedriver.exe que necesita Selenium
+browser = webdriver.Chrome(executable_path=r'path/chromedriver')
+
+#Lista todas las url que extrae de cada página que abre para después acceder a esos links 
+urls_extraidas = []
+
+#Recorre todas las páginas internas por el range del offset de la página de airbnb. Parsea el html
+for n in range(0,17):
+	browser.get('https://www.airbnb.com.ar/s/Buenos-Aires--Ciudad-Aut%C3%B3noma-de-Buenos-Aires/homes?place_id=ChIJvQz5TjvKvJURh47oiC6Bs6A&refinement_paths%5B%5D=%2Fhomes&allow_override%5B%5D=&s_tag=NOZG9x8y&section_offset='+str(n)) #/s/san-telmo/homes')
+	soup = BeautifulSoup(browser.page_source, 'html.parser')
+	
+#Extrae los links de cada aviso, internos a la "home page" solo si contiene el prefijo "rooms"
+	for link in soup.findAll('a', href=True):
+	    x = link.get('href')
+	    if '/rooms' in x:
+	    	print(x)
+	    	urls_extraidas.append(x)
+
+#Compila la regex que extrae el número de aviso que va a usar cuando nombre los archivos
+
+regex = re.compile(r'/rooms/(\d+)\?')
+
+#Abre la página de cada aviso interno a la "home page" y lo guardo como html
+for url in urls_extraidas:
+	matchregex = regex.match(url).group(1)
+	browser.get('https://www.airbnb.com.ar'+str(url))
+	soup = BeautifulSoup(browser.page_source, 'html.parser')
+	with open('path/airbnb/'+str(matchregex)+'.html', 'w') as f:
+		f.write(browser.page_source)
+
+
+#Abre el json de los reviews con el key sacado de la url que devuelve airbnb (buscar en "herramientas del desarrollador" -> Network).
+#Lo abre una vez, busca el count, y setea el offset de cada review, vuelve a entrar cambiando el offset.
+#Lo parsea con la librería json para sacar el reviews count.
+#Se puede hacer seteando de ante mano una cantidad de reviews (por ej. 100)
+
+for url in urls_extraidas:
+	matchregex = regex.match(url).group(1)
+	offset = 0
+	cantidad = 7
+	browser.get('https://www.airbnb.com.ar/api/v2/reviews?key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=ARS&locale=es-419&listing_id='+matchregex+'&role=guest&_format=for_p3&_limit=' + str(cantidad) + '&_offset=' + str(offset) + '&_order=language_country')
+	pre = browser.find_element_by_tag_name("pre").text
+	data = json.loads(pre)
+	count = data['metadata']['reviews_count']
+	with codecs.open('path/airbnb/'+str(matchregex)+'-'+str(offset)+'.json', 'w','utf-8') as ff:
+		ff.write(pre)
+	for n in range(1,int(count/cantidad)+1):
+		offset = n*cantidad
+		browser.get('https://www.airbnb.com.ar/api/v2/reviews?key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=ARS&locale=es-419&listing_id='+matchregex+'&role=guest&_format=for_p3&_limit=' + str(cantidad) + '&_offset=' + str(offset) + '&_order=language_country')
+		pre = browser.find_element_by_tag_name("pre").text
+		data = json.loads(pre)
+		count = data['metadata']['reviews_count']
+		with codecs.open('path/airbnb/'+str(matchregex)+'-'+str(offset)+'.json', 'w','utf-8') as ff:
+			ff.write(pre)
+	
+	
